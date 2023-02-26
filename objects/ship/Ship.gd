cc37721558952650e8d496bd82072507c2198280
Thomas Liske <thomas@fiasko-nw.net>
2023-02-26 14:33:24 +0100

    Ship: convert to RigidBody2D

--- objects/ship/Ship.gd
+++ objects/ship/Ship.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 onready var Bullet = preload("res://objects/weapon/Laser.tscn")
 
@@ -6,7 +6,6 @@ var velocity : Vector2
 export (int) var acceleration = 300
 export (int) var max_speed = 500
 
-var rotation_direction : int
 export (float) var rotation_speed = 3.5
 
 export (float) var slow_down = 0.005
@@ -21,40 +20,38 @@ func _on_Laser_body_entered(body : Node, bullet):
 		bullet.queue_free()
 		emit_signal("laser_has_hit", body)
 
-func _physics_process(delta):
-	rotation_direction = 0
-	if Input.is_action_pressed("rotate_left"):
-		rotation_direction = -1
+var thrust = Vector2(30, 0)
+var torque = 300
+
+func _integrate_forces(state):
+	if Input.is_action_pressed("boost"):
+		$Sprite/CPUParticles2D.emitting = true
+		if linear_velocity.length() < 400:
+			state.apply_central_impulse(thrust.rotated(rotation))
+	else:
+		$Sprite/CPUParticles2D.emitting = false
+		#state.apply_central_impulse(Vector2())
+
+	var rotation_direction = 0
 	if Input.is_action_pressed("rotate_right"):
 		rotation_direction += 1
-	
-	var boost = clamp(Input.get_action_strength("boost"), 0, max_speed)
+	if Input.is_action_pressed("rotate_left"):
+		rotation_direction -= 1
 
-	$Sprite/CPUParticles2D.emitting = boost > 0
-	if $Sprite/CPUParticles2D.emitting:
-		$Sprite/CPUParticles2D.speed_scale = max_speed/boost * 2
+	if rotation_direction < 0 and angular_velocity > -4:
+		state.apply_torque_impulse(-torque)
+	if rotation_direction > 0 and angular_velocity < 4:
+		state.apply_torque_impulse(torque)
 
-	if boost == 0 && velocity != Vector2.ZERO:
-		velocity = lerp(velocity, Vector2.ZERO, slow_down)
-		if abs(velocity.x) <= 0.1 && abs(velocity.y) <= 0:
-			velocity = Vector2.ZERO
-	else:
-		velocity += Vector2(boost * acceleration * delta, 0).rotated(rotation)
-		velocity.x = clamp(velocity.x, -max_speed, max_speed)
-		velocity.y = clamp(velocity.y, -max_speed, max_speed)
-
-	rotation += rotation_direction * rotation_speed * delta
-	velocity = move_and_slide(velocity)
-
-	# wrap around
-	position.x = wrapf(position.x, 0, viewport_size.x)
-	position.y = wrapf(position.y, 0, viewport_size.y)
-
+func _physics_process(_delta):
 	if Input.is_action_just_pressed("shoot"):
 		var bullet = Bullet.instance()
 		get_parent().add_child(bullet)
-		bullet.global_rotation = $Position2D_Center.global_rotation
-		bullet.global_position = $Position2D_Center.global_position
-
+		bullet.global_rotation = $Position2D_Gun.global_rotation
+		bullet.global_position = $Position2D_Gun.global_position
 		bullet.apply_central_impulse(Vector2.RIGHT.rotated(rotation) * 600)
 		bullet.connect("body_entered", self, "_on_Laser_body_entered", [bullet])
+
+func _on_VisibilityNotifier2D_screen_exited():
+	position.x = wrapf(position.x, 0, viewport_size.x)
+	position.y = wrapf(position.y, 0, viewport_size.y)
