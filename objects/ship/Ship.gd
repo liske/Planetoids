9c6a6a3e49e1293aa5e7945f4c9f3999f26c8555
Thomas Liske <thomas@fiasko-nw.net>
2023-02-26 16:01:21 +0100

    Ship: move velocity and torque limits and impulses into export variables

--- objects/ship/Ship.gd
+++ objects/ship/Ship.gd
@@ -3,12 +3,12 @@ extends RigidBody2D
 onready var Bullet = preload("res://objects/weapon/Laser.tscn")
 
 var velocity : Vector2
-export (int) var acceleration = 300
-export (int) var max_speed = 500
 
-export (float) var rotation_speed = 3.5
+export (int) var max_linear_velocity = 400
+export (Vector2) var linear_thrust = Vector2(30, 0)
 
-export (float) var slow_down = 0.005
+export (int) var max_angular_velocity = 4
+export (int) var torque_impulse = 300
 
 onready var viewport_size = get_viewport_rect().size
 
@@ -20,17 +20,13 @@ func _on_Laser_body_entered(body : Node, bullet):
 		bullet.queue_free()
 		emit_signal("laser_has_hit", body)
 
-var thrust = Vector2(30, 0)
-var t = 300
-
 func _integrate_forces(state):
 	if Input.is_action_pressed("boost"):
 		$Sprite/CPUParticles2D.emitting = true
-		if linear_velocity.length() < 400:
-			state.apply_central_impulse(thrust.rotated(rotation))
+		if linear_velocity.length() < max_linear_velocity:
+			state.apply_central_impulse(linear_thrust.rotated(rotation))
 	else:
 		$Sprite/CPUParticles2D.emitting = false
-		#state.apply_central_impulse(Vector2())
 
 	var rotation_direction = 0
 	if Input.is_action_pressed("rotate_right"):
@@ -38,10 +34,10 @@ func _integrate_forces(state):
 	if Input.is_action_pressed("rotate_left"):
 		rotation_direction -= 1
 
-	if rotation_direction < 0 and angular_velocity > -4:
-		state.apply_torque_impulse(-t)
-	if rotation_direction > 0 and angular_velocity < 4:
-		state.apply_torque_impulse(t)
+	if rotation_direction < 0 and angular_velocity > -max_angular_velocity:
+		state.apply_torque_impulse(-torque_impulse)
+	if rotation_direction > 0 and angular_velocity < max_angular_velocity:
+		state.apply_torque_impulse(torque_impulse)
 
 func _physics_process(_delta):
 	# wrap around screen edges
