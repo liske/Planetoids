34a88854f5d918426750e0ab6d44c76e67346443
Thomas Liske <thomas@fiasko-nw.net>
2023-02-27 07:38:58 +0100

    Laser: make it wrapping

--- objects/weapon/Laser.gd
+++ objects/weapon/Laser.gd
@@ -1,4 +1,12 @@
 extends RigidBody2D
 
-func _on_VisibilityNotifier2D_screen_exited():
+onready var viewport_size = get_viewport_rect().size
+
+func _physics_process(_delta):
+	# wrap around screen edges
+	position.x = wrapf(position.x, 0, viewport_size.x)
+	position.y = wrapf(position.y, 0, viewport_size.y)
+
+
+func _on_Timer_timeout():
 	queue_free()
