776ac31b6dfcc6c32984c1d60343213308384555
Thomas Liske <thomas@fiasko-nw.net>
2023-03-10 09:57:03 +0100

    Make highscore recording working.

--- scripts/Highscore.gd
+++ scripts/Highscore.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var HIGHSCORE_FN = 'user://highscore.json'
+var HIGHSCORE_MAX_ENTRIES = 3
 
 func load_highscore():
 	var highscores = []
@@ -16,25 +17,35 @@ func load_highscore():
 
 func add_highscore(score, level, player):
 	var highscores = load_highscore()
-	
-	highscores.append({
+
+	var position = get_highscore_position(score)
+	highscores.insert(position - 1, {
 		'score': score,
 		'level': level,
 		'player': player,
 	})
+
+	if highscores.size() > HIGHSCORE_MAX_ENTRIES:
+		highscores.resize(HIGHSCORE_MAX_ENTRIES)
 
 	var file = File.new()
 	if file.open(HIGHSCORE_FN, File.WRITE) == OK:
 		file.store_string(JSON.print(highscores))
 		file.close()
 
-func check_highscore(score):
+func get_highscore_position(score):
 	var highscores = load_highscore()
+	var pos = highscores.size()
 
-	return true
+	for i in range(0, highscores.size()):
+		if score > highscores[i].score:
+			pos = i
+			break
 
-	for i in highscores:
-		if score > i.score:
-			return true
+	if pos < HIGHSCORE_MAX_ENTRIES:
+		return pos + 1
+	else:
+		return -1
 
-	return false
+func check_highscore(score):
+	return get_highscore_position(score) > -1
